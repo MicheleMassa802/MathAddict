73a3e002f81e6610b597f73b520c06923532dca6
Michele Massa <75578291+MicheleMassa802@users.noreply.github.com>
2025-07-26 10:47:23 -0400

    In page DOM access

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,9 +1,9 @@
 // events that get fired when the extension is clicked, using the open tab
-chrome.action.onClicked.addListener(tab => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func: () => {
-            alert('Hello from MathAddict');
-        }
-    });
-});
+// chrome.action.onClicked.addListener(tab => {
+//     chrome.scripting.executeScript({
+//         target: {tabId: tab.id},
+//         func: () => {
+//             alert('Hello from MathAddict');
+//         }
+//     });
+// });
