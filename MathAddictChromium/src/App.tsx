cd47e2666c91f95cee2d8de580a413a2445b3439
Michele Massa <75578291+MicheleMassa802@users.noreply.github.com>
2025-07-27 12:08:45 -0400

    Working Initial Vite project

--- MathAddictChromium/src/App.tsx
+++ MathAddictChromium/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState} from "react";
 
+// this code executes inside the context of the popup
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [color, setColor] = useState('white');  // stored in popup code
+
+    const onclick = async () => {
+        let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+        chrome.scripting.executeScript<string[], void>({
+            target: {tabId: tab.id!},
+            args: [color],
+            // this function executes inside the context of the active tab
+            // the color from the popup gets passed through the executeScript parameters
+            func: (color) => {
+                document.body.style.backgroundColor = color;
+            }
+        })
+    }
+    return (
+        <>
+            <h1>Math Addict 4 Chromium</h1>
+            <div>
+                <h3>Select a color!</h3>
+                <input type="color" onChange={(e) => setColor(e.currentTarget.value)}/>
+            </div>
+
+            <div className="card">
+                <button onClick={onclick}>
+                    Set Color
+                </button>
+            </div>
+            <p className="read-the-docs">
+                Welcome!
+            </p>
+        </>
+    )
 }
 
 export default App
