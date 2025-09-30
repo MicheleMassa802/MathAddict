0364e16feabc42fbed789eb8c8e52c40295d1da2
Michele Massa <75578291+MicheleMassa802@users.noreply.github.com>
2025-09-30 15:34:36 -0400

    Overcome CSP by keeping script on separate module file

--- MathAddictChromium/src/content.js
+++ MathAddictChromium/src/content.js
@@ -10,11 +10,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         div.textContent = request.text;
         div.id = extensionDivId;
         div.style.position = "fixed";
+        div.style.width = "400px";
+        div.style.height = "711px";
         div.style.bottom = "10px";
         div.style.right = "10px";
         div.style.backgroundColor = "#556B2F";
         div.style.padding = "10px";
         div.style.zIndex = "9999";
+
+        // bring in unity through an iframe to avoid CSP stuff
+        const iframe = document.createElement("iframe");
+        iframe.src = chrome.runtime.getURL("GameBuild/index.html");
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        div.appendChild(iframe);
         document.body.appendChild(div);
 
         divActive = true;
